5528b821a960e8ae5993f2b4026f02037d9fe7e5
darknightmare <rubentkd98@gmail.com>
2015-08-12 17:38:12 +0200

    msm8939-common: Tests tests tests and more radio tests

--- msm8939.mk
+++ msm8939.mk
@@ -218,6 +218,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb
 
+# Data configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
