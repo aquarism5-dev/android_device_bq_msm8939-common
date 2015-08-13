7f4cd0ac2c6f821e8a0420d46039d969bbd6e3d0
darknightmare <rubentkd98@gmail.com>
2015-08-13 14:45:57 +0200

    msm8939-common: Fix ramdisk script path

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -14,6 +14,7 @@ LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.class_main.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 # Init scripts
